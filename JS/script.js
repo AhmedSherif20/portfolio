cd2ef08ffacd1b76738b5582ea7ef57133292079
AhmedSherif20 <aymanmard12@gmail.com>
2021-10-21 14:12:50 +0200

    portfolio section done

--- JS/script.js
+++ JS/script.js
@@ -31,30 +31,34 @@ $('.slider').slick({
     infinite: true,
     arrows: false,
     speed: 300,
+    autoplay: true,
+    autoplaySpeed: 2000,
     slidesToShow: 3,
     slidesToScroll: 1,
     responsive: [
         {
-            breakpoint: 1024,
+            breakpoint: 1400,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
+                slidesToShow: 2,
+                slidesToScroll: 2,
                 infinite: true,
                 dots: true
             }
         },
         {
-            breakpoint: 600,
+            breakpoint: 992,
             settings: {
                 slidesToShow: 1,
-                slidesToScroll: 1
+                slidesToScroll: 1,
+                centerMode: true,
+                centerPadding: '60px',
             }
         },
         {
             breakpoint: 480,
             settings: {
                 slidesToShow: 1,
-                slidesToScroll: 1
+                slidesToScroll: 1,
             }
         }
         // You can unslick at a given breakpoint now by adding:
